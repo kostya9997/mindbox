9f7ac5eb5043ba327314e38a57d49db2533dc514
Костя Мальцев <kostya_maltsev@Mail.ru>
2022-01-11 17:53:36 +0300

    добавил тесты

---
+++ Mindbox.GeometryFiguresTests/Model/CircleTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mindbox.GeometryFigures.Model;
+using System;
+
+namespace Mindbox.GeometryFiguresTests.Model
+{
+    [TestClass()]
+    public class CircleTests
+    {
+        [TestMethod()]
+        public void GetSquareTest()
+        {
+            double radius = -1;
+
+            Circle circle;
+
+            //Проверка, действительно ли будет ошибка при некорректном радиусе.
+            Assert.ThrowsException<ArgumentException>(() => circle = new Circle(radius));
+
+            radius = 2; //Целочисленный радиус
+            circle = new Circle(radius);
+            //Фактически 25.128, по мат. округлению 25.13
+            Assert.AreEqual(25.13, circle.GetSquare());
+
+            radius = 4.894d; //Радиус с плавающей точкой
+            circle = new Circle(radius);
+            Assert.AreEqual(150.49, circle.GetSquare());
+        }
+    }
+}
---
+++ Mindbox.GeometryFiguresTests/Model/TriangleTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mindbox.GeometryFigures.Model.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void GetSquareTest()
+        {
+            //Создание треугольника с некорректными значениями, вызывает ошибку.
+            Assert.ThrowsException<ArgumentException>(() => new Triangle(5, 10, 50));
+
+            //Равносторонний треугольник
+            Triangle f = new Triangle(5d, 5d, 5d);
+            //Фактически 10.825, по мат. округлению 10.83
+            Assert.AreEqual(10.83d, f.GetSquare());
+
+            //Остроугольный треугольник
+            f = new Triangle(12d, 12d, 8d);
+            Assert.AreEqual(45.25d, f.GetSquare());
+        }
+
+        [TestMethod()]
+        public void GetIsRightTriangleTest()
+        {
+            double side1 = 5d, side2 = 5d, side3 = 5d;
+
+            //Равносторонний треугольник
+            Triangle f = new Triangle(side1, side2, side3);
+            Assert.AreEqual(false, f.GetIsRightTriangle());
+
+            side1 = 5d;
+            side2 = 5d;
+            side3 = Math.Round(Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2)), 2);
+
+            Assert.AreEqual(7.07d, side3);//Проверка 3 стороны
+
+            var sides = new List<double>() { side1, side2, side3 };
+            double hypotenuse = sides.Max(); //Получение гипотенузы
+
+            Assert.AreEqual(side3, hypotenuse);//Убеждаемся что 3 сторона и есть гипотенуза
+            Assert.AreEqual(50.0d, Math.Round(Math.Pow(hypotenuse, 2), 0));//Убеждаемся что квадрат гипотенузы == 50
+
+            //Создаём прямоугольный треугольник
+            f = new Triangle(side1, side2, side3);
+            Assert.AreEqual(false, f.GetIsRightTriangle()); //Убеждаемся что треугольник действительно прямоугольный
+        }
+    }
+}
